55f38f2a1385faaf2d6fad964719e2e5e36da742
archiProg <baramee.panarchi@gmail.com>
2025-12-22 14:25:39 +0700

    update: icon

--- app/doctor/_layout.tsx
+++ app/doctor/_layout.tsx
@@ -1,23 +1,20 @@
 import { FontAwesome } from '@expo/vector-icons';
-import { Label, NativeTabs } from 'expo-router/unstable-native-tabs';
-import { useColorScheme } from 'react-native';
+import { Icon, Label, NativeTabs, VectorIcon } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <NativeTabs>
+    <NativeTabs >
       <NativeTabs.Trigger name="doctor_home">
         <Label>Home</Label>
-        <FontAwesome name="angle-left" size={24} color={colorScheme === "dark" ? "#fff" : "#444"} />
+        <Icon src={<VectorIcon family={FontAwesome} name="home" />} />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="doctor_history">
         <Label>History</Label>
-        <FontAwesome name="angle-left" size={24} color={colorScheme === "dark" ? "#fff" : "#444"} />
+        <Icon src={<VectorIcon family={FontAwesome} name="history" />} />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="setting">
-        <FontAwesome name="angle-left" size={24} color={colorScheme === "dark" ? "#fff" : "#444"} />
         <Label>Settings</Label>
+        <Icon src={<VectorIcon family={FontAwesome} name="gear" />} />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
